1e0fe1601a27ec36b5fd5edca7345657c0889e39
Brayan García <bgarcia.95@outlook.com>
2019-11-20 00:38:17 -0600

    working with arrays

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,78 +5,103 @@ console.log("App.js is running!");
 var app = {
   title: "Indecision App",
   subtitle: "This is some text",
-  options: ["One", "Two"]
+  options: []
 };
 
-// only render the subtitle and p tag if subtitle exist - logical && operator
-// render new p tag - if options.length > 0 "Here are your options" "No options"
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  // console.log('form submitted');
+  // target -> parent tag (form in this case)
+  // list all the elements and index by name(that's why we use option after 'elements.')
+  // here we are reading the value
+  var option = e.target.elements.option.value;
 
-// JSX - JavaScript XML
-var template = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
-    null,
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    "p",
-    null,
-    app.subtitle
-  ),
-  React.createElement(
-    "p",
-    null,
-    app.options.length > 0 ? "Here are your options" : "No options"
-  ),
-  React.createElement(
-    "ol",
-    null,
-    React.createElement(
-      "li",
-      null,
-      "Item one"
-    ),
-    React.createElement(
-      "li",
-      null,
-      "Item two"
-    )
-  ),
-  React.createElement(
-    "form",
-    null,
-    React.createElement("input", { type: "text", name: "option" }),
-    React.createElement(
-      "button",
-      null,
-      "Add Option"
-    )
-  )
-);
+  if (option) {
+    app.options.push(option);
+    console.log(app.options);
+    e.target.elements.option.value = "";
+    renderApp();
+  }
+};
+
+// Create "Remove ALl" button above list
+// onClick -> wipe  the array -> rerender
+var onRemoveAll = function onRemoveAll() {
+  app.options = [];
+  renderApp();
+};
 
 var appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
 
-// Create a templateTwo var JSX
+var numbers = [55, 101, 1000];
 
-// const user = {
-//   name: "Brayan",
-//   age: 25,
-//   location: "Santa Ana"
-// };
+var id = 0;
 
-// function getLocation(location) {
-//   if (location) {
-//   return <p>Location: {location} </p>;
-//   }
-// }
+var renderApp = function renderApp() {
 
-// var templateTwo = (
-//   <div>
-//     <h1>{user.name ? user.name : 'Anonymous'}</h1>
-//     {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-//     {getLocation(user.location)}
-//   </div>
-// );
+  // JSX - JavaScript XML
+  var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      null,
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      "p",
+      null,
+      app.subtitle
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length > 0 ? "Here are your options" : "No options"
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length
+    ),
+    React.createElement(
+      "button",
+      { onClick: onRemoveAll },
+      "Remove All"
+    ),
+    // JS expression can only accept strings, numbers, booleans, undefined and null
+    numbers.map(function (number) {
+      return React.createElement(
+        "p",
+        { key: number },
+        "Number: ",
+        number
+      );
+    }),
+    React.createElement(
+      "ol",
+      null,
+      app.options.map(function (option) {
+
+        return React.createElement(
+          "li",
+          { key: id++ },
+          option
+        );
+      })
+    ),
+    React.createElement(
+      "form",
+      { onSubmit: onFormSubmit },
+      React.createElement("input", { type: "text", name: "option" }),
+      React.createElement(
+        "button",
+        null,
+        "Add Option"
+      )
+    )
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+renderApp();
--- src/app.js
+++ src/app.js
@@ -3,56 +3,68 @@ console.log("App.js is running!");
 const app = {
   title: "Indecision App",
   subtitle: "This is some text",
-  options: ["One", "Two"]
+  options: []
 };
 
 const onFormSubmit = e => {
   e.preventDefault();
-  console.log('form submitted');
-  
+  // console.log('form submitted');
+  // target -> parent tag (form in this case)
+  // list all the elements and index by name(that's why we use option after 'elements.')
+  // here we are reading the value
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    console.log(app.options);
+    e.target.elements.option.value = "";
+    renderApp();
+  }
 };
 
-// only render the subtitle and p tag if subtitle exist - logical && operator
-// render new p tag - if options.length > 0 "Here are your options" "No options"
-
-// JSX - JavaScript XML
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-    </ol>
-    <form onSubmit={onFormSubmit}>
-      <input type="text" name="option" />
-      <button>Add Option</button>
-    </form>
-  </div>
-);
+// Create "Remove ALl" button above list
+// onClick -> wipe  the array -> rerender
+const onRemoveAll = () => {
+  app.options = [];
+  renderApp();
+};
 
 const appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
 
-// Create a templateTwo var JSX
+const numbers = [55, 101, 1000];
 
-// const user = {
-//   name: "Brayan",
-//   age: 25,
-//   location: "Santa Ana"
-// };
+let id = 0;
 
-// function getLocation(location) {
-//   if (location) {
-//   return <p>Location: {location} </p>;
-//   }
-// }
+const renderApp = () => {
+  
+  // JSX - JavaScript XML
+  const template = (
+    
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
+      <p>{app.options.length}</p>
+      <button onClick={onRemoveAll}>Remove All</button>
+      {// JS expression can only accept strings, numbers, booleans, undefined and null
+      numbers.map(number => {
+        return <p key={number}>Number: {number}</p>;
+      })}
 
-// var templateTwo = (
-//   <div>
-//     <h1>{user.name ? user.name : 'Anonymous'}</h1>
-//     {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-//     {getLocation(user.location)}
-//   </div>
-// );
+      <ol>
+        {app.options.map(option => {
+          
+          return <li key={id++}>{option}</li>
+        })}
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+renderApp();
