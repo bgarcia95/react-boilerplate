e139707d45e1008e8c45ebf8b692018a36763b3d
Brayan Garcia <bgarcia95@outlook.com>
2019-11-30 12:35:36 -0600

    adding normalize to our project

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles/styles.scss";
+import "normalize.css/normalize.css";
 import IndecisionApp from "./components/IndecisionApp";
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
