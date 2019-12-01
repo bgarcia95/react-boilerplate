1e1a2a76b70e64dc215e4bfbbda78aae908c0309
Brayan García <bgarcia.95@outlook.com>
2019-12-01 13:38:18 -0600

    Boilerplate created

--- src/app.js
+++ src/app.js
@@ -4,6 +4,5 @@ import ReactDOM from "react-dom";
 
 import "./styles/styles.scss";
 import "normalize.css/normalize.css";
-import IndecisionApp from "./components/IndecisionApp";
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById("app"));
